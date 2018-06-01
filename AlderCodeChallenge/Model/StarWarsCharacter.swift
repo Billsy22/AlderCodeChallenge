0c60ee8c22dce337d534e0ef0e2eab13028dc42e
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-06-01 16:10:13 -0600

    Initial setup of 'StarWarsCharacter' model object and conformed to Codable

---
+++ AlderCodeChallenge/Model/StarWarsCharacter.swift
@@ -0,0 +1,37 @@
+//
+//  StarWarsCharacter.swift
+//  AlderCodeChallenge
+//
+//  Created by Taylor Bills on 6/1/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct StarWarsCharacter: Codable {
+    
+    // MARK: -  Properties
+    let id: Int?
+    let firstName: String?
+    let lastName: String?
+    let birthdate: String?
+    let profilePicture: String?
+    let forceSensitive: Bool?
+    let affiliation: String?
+    
+    // MARK: -  Coding Keys
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case firstName
+        case lastName
+        case birthdate
+        case profilePicture
+        case forceSensitive
+        case affiliation
+    }
+}
+
+struct CharacterList: Codable {
+    let individuals: [StarWarsCharacter]
+}
